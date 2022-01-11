3ef62ca42ffe854e8ff0e11f5e165b95772ee306
zero0205 <etety39@naver.com>
2022-01-11 13:36:26 +0900

    DFS/BFS 미로탈출

--- DFSBFS_미로 탈출.py
+++ DFSBFS_미로 탈출.py
@@ -7,11 +7,28 @@ graph = []
 for i in range(n):
   graph.append(list(map(int,input())))
 
-cnt = 2 # 시작 칸과 마지막 칸 포함
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+deq = deque([(0,0)])
 
 # 아직 방문 안 한 노드면 큐에 넣고 
-def bfs(x, y):
-    # 출구 도착?
-    if x == n and y == m:
-        return cnt
-DFS/BFS_미로 탈출(미완)
+def bfs():
+  while deq:
+    x, y = deq.popleft()
+    # 상하좌우로 이동가능한지 탐색
+    # 이동 가능한 칸은 큐에 넣어주고 거기까지의 거리로 값을 변경
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      # 범위 안에 있는지?
+      if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        continue
+      # 갈 수 있는 칸이면 그 칸까지의 거리를 넣어줌. 이미 지나온 칸이면 값이 1이 아닐것
+      if graph[nx][ny] == 1:
+        deq.append((nx,ny))
+        graph[nx][ny] = graph[x][y] + 1
+
+bfs()    
+
+print(graph[n-1][m-1])
