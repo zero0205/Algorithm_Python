ee8f441cddf05bfe7058cbc4d3eb56e61153f4a7
zero0205 <etety39@naver.com>
2022-01-19 16:47:48 +0900

    이진탐색_떡볶이 떡 만들기

--- 이진탐색_떡볶이 떡 만들기.py
+++ 이진탐색_떡볶이 떡 만들기.py
@@ -1,11 +1,32 @@
 # n : 떡의 개수, m : 손님이 요청한 떡의 길이
+from functools import total_ordering
+import re
+from unittest import result
+
+
 n, m = map(int, input().split())
 ddeok = list(map(int, input().split()))
 
-mid = min(ddeok)
-cnt = len(ddeok)
+start = min(ddeok)
+end = max(ddeok)
 
-while True:
-    sum_d = sum(ddeok) - cnt * mid
+cnt = len(ddeok)
+res = 0
+
+while start <= end:
+    sum = 0
+    mid = (start + end) // 2
     
-    if m == sum_d
+    for i in ddeok:
+        if i < mid:
+            continue
+        sum += (i - mid)
+        
+    # 원하는 양보다 적게 잘라짐 -> 높이 내려야지
+    if sum < m:
+        end = mid - 1
+    # 원하는 양보다 많이 잘라짐 -> 높이 올려야지
+    else:
+        res = mid
+        start = mid + 1
+        
