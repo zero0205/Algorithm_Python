11e1369a08904674f2ee0500a8400299a91d29a3
zero0205 <etety39@naver.com>
2022-03-13 10:51:11 +0900

    오타 수정

--- part3/DP_병사 배치하기.py
+++ part3/DP_병사 배치하기.py
@@ -27,7 +27,7 @@ array.reverse()
 # 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
 dp = [1] * n
 
-# 가장 긴 증가하는 부분 수열(LTS) 알고리즘 수행
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
 for i in range(1, n):
     for j in range(0, i):
         if array[j] < array[i]:
