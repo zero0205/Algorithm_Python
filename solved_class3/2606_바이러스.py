d3ff6438fb6b4d2efde0c3d7f1198bbdfffe2d18
zero0205 <etety39@naver.com>
2022-03-14 12:33:27 +0900

    백준 2606번 푸는 중

---
+++ solved_class3/2606_바이러스.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/2606
+
+import sys
+from collections import deque
+from unittest import result
+
+input = sys.stdin.readline
+
+def bfs(graph):
+    q = deque([1])
+    result = 0
+    visited = [False] * (n + 1)
+    visited[1] = True
+    while q:
+        nx = q.pop()
+        for node in graph[nx]:
+            if not visited[node]:
+                result += 1
+                q.append(nx)  
+                visited[node] = True
+    return result
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+print(bfs(graph))
