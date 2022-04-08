0a913d61fd4b2e4c8667f2990c2eb2ef28d001bb
zero0205 <etety39@naver.com>
2022-04-08 22:13:29 +0900

    4/7 백준 3문제 풀이

--- solved_class4/9465_스티커.py
+++ solved_class4/9465_스티커.py
@@ -1,16 +1,20 @@
 # https://www.acmicpc.net/problem/9465
 
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     n = int(input())
     sticker = []
     sticker.append(list(map(int, input().split())))
     sticker.append(list(map(int, input().split())))
-    
-    sticker[0][1] += sticker[1][0]
-    sticker[1][1] += sticker[0][0]
 
-    for i in range(2, n):
-        sticker[0][i] += max(sticker[1][i-2], sticker[1][i-1])
-        sticker[1][i] += max(sticker[0][i-2], sticker[0][i-1])
+    for i in range(1, n):
+        if i == 1:
+            sticker[0][i] += sticker[1][i-1]
+            sticker[1][i] += sticker[0][i-1]
+        else:
+            sticker[0][i] += max(sticker[1][i-2], sticker[1][i-1])
+            sticker[1][i] += max(sticker[0][i-2], sticker[0][i-1])
         
     print(max(sticker[0][n-1], sticker[1][n-1]))
