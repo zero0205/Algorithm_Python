9b6828d519b4377c784582296c3a4dc809531df2
zero0205 <etety39@naver.com>
2022-07-07 10:30:17 +0900

    7/6에 풀던 1929번 해결

--- baekjoon/1929_소수 구하기.py
+++ baekjoon/1929_소수 구하기.py
@@ -2,14 +2,18 @@
 
 m, n = map(int, input().split())
 
-# 에라토라테네스의 체
+# 에라토스테네스의 체
 # 소수를 판별할 범위만큼의 배열을 할당하여, 해당하는 값을 넣어주고, 하나씩 지워감
 
 lst = [True for _ in range(n+1)]
-
-for i in range(2, n + 1):
+lst[1] = False
+# 왜 i + 1의 제곱근까지만?
+# => n의 최대 약수가 n의 제곱근 이하이므로 거기까지만 확인해도 됨
+for i in range(2, int(n ** 0.5) + 1):
     if lst[i]:
-        for j in range(i * 2, n + 1, i):   # 자기 자신은 빼고 배수들만 처리
+        # 자기 자신은 빼고 배수들만 처리. 
+        # 이때 i * i 이전의 i의 배수들은 이미 처리되었으므로 확인하지 않아도 됨
+        for j in range(i * i, n + 1, i):   
             lst[j] = False
 
 for idx in range(m, n+1):
