4c87998bd83388116f89d789091cf084edfdc67f
zero0205 <etety39@naver.com>
2023-07-21 16:20:18 +0900

    [Modify] BOJ 1695 - 팰린드롬 만들기

--- baekjoon/DP/1695_팰린드롬 만들기.py
+++ baekjoon/DP/1695_팰린드롬 만들기.py
@@ -1,33 +1,9 @@
-# n = int(input())
-# arr = list(map(int, input().split()))
-
-# if n == 1:
-#     print(0)
-#     exit()
-# if n == 2:
-#     print(0 if arr[0] == arr[1] else 1)
-#     exit()
-    
-# ans = 5000
-# # 하나씩 거꾸로 가며 확인
-# for i in range(n):
-#     prefix, suffix = i, i+2
-#     num = 0
-#     while suffix < n:
-#         if arr[suffix] == arr[prefix]:
-#             suffix += 1
-#             num += 1
-#         prefix -= 1
-#         if suffix == n:
-#             ans = min(ans, i+1-num)
-#         if prefix < 0:
-#             break
-# print(ans)
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [[0] * (n+1) for _ in range(n+1)]  # arr i번째까지의 수열과 arr의 역순수열의 j번째까지의 수열의 최장공통수열 길이 저장
+# arr i번째까지의 수열과 arr의 역순수열의 j번째까지의 수열의 최장공통부분수열(LCS) 길이 저장
+dp = [[0] * (n+1) for _ in range(n+1)]  
+
 for i in range(1, n+1):
     for j in range(1, n+1):
         if arr[i-1] == arr[n-j]:
