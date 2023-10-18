7b2e87d3aeebf94dae8c7e37ac15552cf4eeaef4
zero0205 <etety39@naver.com>
2023-10-18 14:45:22 +0900

    [Modify] Apply Prettier

--- solved_class3/1260_BFS와 DFS.py
+++ solved_class3/1260_BFS와 DFS.py
@@ -3,6 +3,7 @@
 # DFS
 from collections import deque
 
+
 def dfs(v, visited):
     visited[v] = True
     print(v, end=" ")
@@ -12,11 +13,13 @@ def dfs(v, visited):
     return
 
 # BFS
+
+
 def bfs(start):
     q = deque([start])
     visited = [False] * (n + 1)
     visited[start] = True
-    
+
     while q:
         x = q.popleft()
         print(x, end=" ")
@@ -24,7 +27,8 @@ def bfs(start):
             if not visited[node]:
                 q.append(node)
                 visited[node] = True
-    
+
+
 # n: 정점의 개수, m: 간선의 개수, v: 시작 번호
 n, m, v = map(int, input().split())
 
@@ -34,11 +38,11 @@ for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    
+
 # 방문할 수 있는 정점이 여러 개인 경우 번호가 작은 것부터 방문하기 위해 정렬
 for lst in graph:
     lst.sort()
-    
+
 visited = [False] * (n + 1)
 dfs(v, visited)
 print()
