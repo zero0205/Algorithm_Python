ff5b03fb596e6a6221c645c0d773f4cbcd25eb22
zero0205 <etety39@naver.com>
2024-01-04 14:35:44 +0900

    [BOJ] 4485 - 녹색 옷 입은 애가 젤다지?

---
+++ baekjoon/ShortestPath/4485_녹색 옷 입은 애가 젤다지.py
@@ -0,0 +1,27 @@
+from heapq import heappop, heappush
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+idx = 1
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    board = [list(map(int, input().split())) for _ in range(n)]
+
+    losts = [[int(1e9)]*n for _ in range(n)]
+    losts[0][0] = board[0][0]
+
+    q = [(0, 0)]
+    while q:
+        x, y = heappop(q)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and losts[nx][ny] > losts[x][y]+board[nx][ny]:
+                heappush(q, (nx, ny))
+                losts[nx][ny] = losts[x][y]+board[nx][ny]
+    print(f"Problem {idx}: {losts[n-1][n-1]}")
+    idx += 1
