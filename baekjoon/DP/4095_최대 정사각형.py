33c6ef90a0de1ff525f4f84f64722e3fc7dab9ab
zero0205 <etety39@naver.com>
2024-02-10 13:48:49 +0900

    [BOJ] 4095 - 최대 정사각형

---
+++ baekjoon/DP/4095_최대 정사각형.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    ans = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if matrix[i-1][j-1] == 1:
+                dp[i][j] = min([dp[i-1][j], dp[i-1][j-1], dp[i][j-1]])+1
+            if dp[i][j] > ans:
+                ans = dp[i][j]
+    print(ans)
