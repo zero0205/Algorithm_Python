6c4a666e2a924c197aacda7cbfb2039aa7e46edd
zero0205 <etety39@naver.com>
2024-09-16 16:20:41 +0900

    [BOJ] 5073 - 삼각형과 세 변

---
+++ baekjoon/Math/5073_삼각형과 세 변.py
@@ -0,0 +1,12 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    if a == b and b == c:
+        print("Equilateral")
+    elif (max([a, b, c]) >= sum([a, b, c])-max([a, b, c])):
+        print("Invalid")
+    elif (a == b and b != c) or (b == c and a != c) or (a == c and b != a):
+        print("Isosceles")
+    else:
+        print("Scalene")
